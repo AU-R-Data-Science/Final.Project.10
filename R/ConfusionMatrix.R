18d41339d8ab9bb782f50b52dd6743d47c1a3f79
LeshanZhao <117010401@link.cuhk.edu.cn>
2022-11-30 00:21:14 -0600

    cutoff_logistic_confusionMatrix Done

--- R/ConfusionMatrix.R
+++ R/ConfusionMatrix.R
@@ -1,1 +1,37 @@
 
+confusion.matrix <- function(pred.value, actual.value, cutoff=0.5){
+  Confusion <- data.frame(pred=pred.value, actual=actual.value)
+  Confusion[Confusion >= cutoff]=1
+  Confusion[Confusion < cutoff]=0
+  Confusion[,c(1,2)]=lapply
+
+  N = nrow(Confusion)
+  TP = nrow(Confusion[Confusion$pred=="1" & Confusion$actual=="1", ])
+  TN = nrow(Confusion[Confusion$pred=="0" & Confusion$actual=="0", ])
+  FP = nrow(Confusion[Confusion$pred=="1" & Confusion$actual=="0", ])
+  FN = nrow(Confusion[Confusion$pred=="0" & Confusion$actual=="1", ])
+
+  Accuracy <- (TP+TN)/N
+  Prevalence <- (TP+FN)/N
+  Sensitivity <- TP/(TP+FN)
+  Specificity <- TN/(TN+FP)
+  False.Discovery.Rate <- FP/(TP+FP)
+  Diagnostic.Odds.Ratio <- (TP*TN)/(FN*FP)
+
+  metrics <- c(Prevalence,Accuracy,Sensitivity,Specificity,False.Discovery.Rate,Diagnostic.Odds.Ratio)
+  names(metrics) <-c("Prevalence",
+                     "Accuracy",
+                     "Sensitivity",
+                     "Specificity",
+                     "False.Discovery.Rate",
+                     "Diagnostic.Odds.Ratio")
+
+  matrix <- c(TP,FP,FN,TN)
+  names(matrix) <- c("TP","FP","FN","TN")
+
+  output.list <- list("metrics" = metrics,
+                      "matrix" = matrix)
+  return(output.list)
+}
+
+
